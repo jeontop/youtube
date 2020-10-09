e38a331512775914484b1220f3ffd04e0d7e5200
jeontop <70100432+jeontop@users.noreply.github.com>
2020-10-10 03:43:05 +0900

    휴

---
+++ P200922/src/P5.java
@@ -0,0 +1,24 @@
+
+public class P5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {1,2,3,4,5,6,7,8};
+		
+		/*
+		 * for(int i = 0; i < arr.length ; i++) {
+		 * 
+		 * 
+		 * }
+		 */
+		
+		for(int x : arr) {     //향상된 for문
+			
+			System.out.println(x);
+			
+		}
+		
+	}
+
+}
